43e5e5609e03d5bc5ea812998f16bb09f18438c4
muhammadkoku <61241005+muhammadkoku@users.noreply.github.com>
2020-06-06 21:08:38 +0700

    make it better

--- src/main/java/MuhammadKoku/jfood/DatabaseConnectionPostgre.java
+++ src/main/java/MuhammadKoku/jfood/DatabaseConnectionPostgre.java
@@ -8,7 +8,7 @@ public class DatabaseConnectionPostgre {
         Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/jfood", "postgres", "");
+            c = DriverManager.getConnection("jdbc:postgresql://ec2-34-197-141-7.compute-1.amazonaws.com:5432/d7op1ucac22oin", "lcfimrcbqkmbyv", "10985f701fde797b3f3522d2eebf289444e0c79f71f450fed5290febf1a3e6c7");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
